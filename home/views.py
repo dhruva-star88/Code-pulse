4ebd89ba74bb77f00d584bd0a8ab8ba82d94787c
Swathi Kulkarni <swathik012004@gmail.com>
2024-06-07 15:37:04 +0530

    first commit

--- home/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth.models import User
-
-def home(requests):
-    return render(requests, "test.html")
--- patient_details/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import PatientData
-
-class PatientDataAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', )
-
-
-admin.site.register(PatientData)
-
